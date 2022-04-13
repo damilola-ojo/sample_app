0e946716fb96f9c14f4b7257cd7d33095e4c82f7
damilola-ojo <tdami262@gmail.com>
2022-04-13 22:00:40 +0100

    Implement basic login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,9 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Suppress logger output for asset requests.
+
   config.assets.quiet = true
+  config.sass.inline_source_maps = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
